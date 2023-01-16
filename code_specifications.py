17b7a0d730a5066f7a5182431b5c023fa6b104b1
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-01-15 21:50:23 -0600

    Buenas practicas en programación, uso de "Code Specifications"

---
+++ code_specifications.py
@@ -0,0 +1,11 @@
+# Este es un ejemplo de "code specification" o "docstring    
+
+def suma(a, b):
+    """Suma dos valores a y b.
+    parametro int a cualquier entero
+    parametro int b cualquier entero
+    returns la sumatoria de a y b
+    """
+
+    total = a + b
+    return total
